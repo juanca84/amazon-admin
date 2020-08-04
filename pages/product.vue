90cec255d8307556012bb208f713a0e92841ef63
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2020-08-04 00:02:35 -0400

    Adición de formulario para adicionar productos

---
+++ pages/product.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-6">
+                    <div class="a-section">
+                        a-spacing
+                        <h2 style="text-align: center">Add a new Product</h2>
+                        <form>
+                            <!-- Category Dropdown -->
+                            <div class="a-spacing-top-medium">
+                                <label>Category</label>
+                                <select class="a-select-option">
+                                     <option value="1">1</option>
+                                     <option value="2">2</option>
+                                </select>
+                            </div>
+                            <!-- Owner Dropdown -->
+                            <div class="a-spacing-top-medium">
+                                <label>Category</label>
+                                <select class="a-select-option">
+                                     <option value="1">1</option>
+                                     <option value="2">2</option>
+                                </select>
+                            </div>
+                            <!-- Title dropdown -->
+                            <div class="a-spacing-top-medium">
+                                <label style="margin-bottom: 0px">Title</label>
+                                <input type="text" class="a-input-text" style= "width: 100%">
+                            </div>
+                            <!-- Price dropdown -->
+                            <div class="a-spacing-top-medium">
+                                <label style="margin-bottom: 0px">Price</label>
+                                <input type="number" class="a-input-text" style= "width: 100%">
+                            </div>
+
+                            <!-- Description dropdown -->
+                            <div class="a-spacing-top-medium">
+                                <label style="margin-bottom: 0px">Description</label>
+                                <textarea placeholder="provide deatails such as a product description" style="width: 100%"></textarea>
+                            </div>
+                            <!-- Photo file -->
+                            <div class="a-spacing-top-medium">
+                                <label style="margin-bottom: 0px">Add Photo</label>
+                                <div class="a-row a-spacing-top-medium">
+                                    <label class="choosefile-button">
+                                        <i class="fal fa-plus"></i>
+                                        <input type="file">
+                                        <p style="margin-top: -70px">name of the</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- Button -->
+                            <hr/>
+                            <div class="a-spacing-top-large">
+                                <span class="a-button-register">
+                                    <span class="a-button-inner">
+                                        <span class="a-button-text">Add product</span>
+                                    </span>
+                                </span>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
+        </div>    
+    </main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
