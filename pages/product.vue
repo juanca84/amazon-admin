d09fab0698b817ca74475d506eec7201972bcfa6
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2020-08-04 23:07:35 -0400

    Adición de métodos para persistir datos

--- pages/product.vue
+++ pages/product.vue
@@ -11,32 +11,38 @@
                             <!-- Category Dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label>Category</label>
-                                <select class="a-select-option">
+                                <select class="a-select-option" v-model="categoryID">
                                     <option v-for="category in categories" :value="category._id" :key="category._id">{{category.type}}</option>
                                 </select>
                             </div>
                             <!-- Owner Dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label>Owner</label>
-                                <select class="a-select-option">
+                                <select class="a-select-option" v-model="ownerID">
                                     <option v-for="owner in owners" :value="owner._id" :key="owner._id">{{owner.name}}</option>
                                 </select>
                             </div>
                             <!-- Title dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label style="margin-bottom: 0px">Title</label>
-                                <input type="text" class="a-input-text" style= "width: 100%">
+                                <input type="text" class="a-input-text" style= "width: 100%" v-model="title">
                             </div>
                             <!-- Price dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label style="margin-bottom: 0px">Price</label>
-                                <input type="number" class="a-input-text" style= "width: 100%">
+                                <input type="number" class="a-input-text" style= "width: 100%" v-model="price">
+                            </div>
+
+                            <!-- stockQuantity dropdown -->
+                            <div class="a-spacing-top-medium">
+                                <label style="margin-bottom: 0px">Stock Quantity</label>
+                                <input type="number" class="a-input-text" style= "width: 100%" v-model="stockQuantity">
                             </div>
 
                             <!-- Description dropdown -->
                             <div class="a-spacing-top-medium">
                                 <label style="margin-bottom: 0px">Description</label>
-                                <textarea placeholder="provide deatails such as a product description" style="width: 100%"></textarea>
+                                <textarea placeholder="provide deatails such as a product description" style="width: 100%" v-model="description"></textarea>
                             </div>
                             <!-- Photo file -->
                             <div class="a-spacing-top-medium">
@@ -44,8 +50,8 @@
                                 <div class="a-row a-spacing-top-medium">
                                     <label class="choosefile-button">
                                         <i class="fal fa-plus"></i>
-                                        <input type="file">
-                                        <p style="margin-top: -70px">name of the</p>
+                                        <input type="file" @change="onFileSelectd">
+                                        <p style="margin-top: -70px">{{fileName}}</p>
                                     </label>
                                 </div>
                             </div>
@@ -54,7 +60,7 @@
                             <div class="a-spacing-top-large">
                                 <span class="a-button-register">
                                     <span class="a-button-inner">
-                                        <span class="a-button-text">Add product</span>
+                                        <span class="a-button-text" @click="onAddProduct">Add product</span>
                                     </span>
                                 </span>
                             </div>
@@ -88,6 +94,39 @@
                 }   
             } catch (err) {
                 console.log(err);
+            }
+        },
+        data () {
+            return {
+                categoryID: null,
+                ownerID: null,
+                title: "",
+                price: 0,
+                description: "",
+                selectedFile: null,
+                fileName:"",
+                stockQuantity: 1
+            }
+        },
+        methods: {
+            onFileSelectd(event) {
+                this.selectedFile = event.target.files[0];
+                console.log(this.selectedFile);
+                this.fileName = event.target.files[0].name;
+            },
+            async onAddProduct(){
+                let data = new FormData();
+                data.append("title", this.title);
+                data.append("price", this.price);
+                data.append("description", this.description);
+                data.append("stockQuantity", this.stockQuantity);
+                data.append("ownerID", this.ownerID);
+                data.append("categoryID", this.categoryID);
+                data.append("photo", this.selectedFile, this.selectedFile.name);
+
+                let result = await this.$axios.$post('http://localhost:3000/api/products', data);
+
+                this.$router.push("/")
             }
         }
     }
